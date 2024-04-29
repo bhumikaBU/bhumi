eeda38de2317c215a3080811002de39cb8927b3d
alien <bubhumika881@gamil.com>
2024-04-28 22:20:16 -0700

    v1

---
+++ insertion.py
@@ -0,0 +1,13 @@
+def insort(l)
+    for i in range(1,len)
+      key=l[i]
+      j=i-1
+    while j>=0 and key<l[j]
+      l[j+1]=l(j)
+      j=j-1
+l=[54,64,82,12,7,78]
+print("before sorting list is l") 
+insort(l)
+print(l)
+for i and range(len(l))
+print("%d"%d%l[i])
